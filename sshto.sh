995dcfaaa2d5baa3231a861cca3233716b79eb60
Sridhar Gaddam <sgaddam@redhat.com>
2018-06-26 02:17:27 -0400

    Updated the sshto file with an example

--- sshto.sh
+++ sshto.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 # Credit Josh
 
-. stackrc admin admin
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <operation> <command>
+       Example: ./sshto.sh compute-1  \"/usr/sbin/ip  a | grep 172.17.2.\""
+    exit 1
+fi
+
+
+. ~/stackrc admin admin
 name=$1
 shift
 
 if [ $name == controllers ]
 then
-        name="controller-0 controller-1 controller-2"
+    name="controller-0 controller-1 controller-2"
 fi
 for i in $name
 do
-        echo ssh\'ing to $i 
-        ssh heat-admin@$(nova list 2>/dev/null | grep $i | grep -o "192.168[^ ]*") $@
+    echo ssh\'ing to $i 
+    ssh heat-admin@$(nova list 2>/dev/null | grep $i | grep -o "192.168[^ ]*") $@
 done
 
